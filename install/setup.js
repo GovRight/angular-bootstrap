fe1f45dc6818d251c75509f25a4dade87ff1a381
Victor <victor.miletskiy@gmail.com>
2016-06-25 12:14:58 +0300

    Inquring contributor info

--- install/setup.js
+++ install/setup.js
@@ -6,12 +6,21 @@ var fs = require('fs');
 console.log('\x1b[32mWelcome to the GovRight project wizard!\x1b[0m\n');
 console.log('Please enter required data:');
 
-exec('git config user.name && git config user.email && git config --get remote.origin.url', function (error, stdout, stderr) {
-  var gitInfo = stdout.trim().split('\n');
+exec('git config --get remote.origin.url', function (error, stdout) {
   var questions = [
     {
+      name: 'contributorName',
+      message: 'Your name:',
+      type: 'input'
+    },
+    {
+      name: 'contributorEmail',
+      message: 'Your email:',
+      type: 'input'
+    },
+    {
       name: 'name',
-      message: 'Project name (e.g., My awesome angular project):',
+      message: 'Project name (e.g., Corruption reporting tool):',
       type: 'input'
     },
     {
@@ -34,63 +43,86 @@ exec('git config user.name && git config user.email && git config --get remote.o
     }
   ];
 
+  var gitInfo = stdout.trim().split('\n');
+  if(!gitInfo[0]) {
+    questions.unshift({
+      name: 'repo',
+      message: 'Project repository url (e.g. github or bitbucket link):',
+      type: 'input'
+    });
+  }
+
   inquirer.prompt(questions).then(function (answers) {
     var packageJson = JSON.parse(fs.readFileSync('./install/package.json').toString());
     var bowerJson = JSON.parse(fs.readFileSync('./bower.json').toString());
     var readmeMd = fs.readFileSync('./install/README.md').toString();
-    answers.name = answers.name.trim();
     answers.port = parseInt(answers.port);
+    ['name', 'repo', 'contributorName', 'contributorEmail'].forEach(function(k) {
+      if(answers[k]) {
+        answers[k] = answers[k].trim();
+      }
+    });
+    var contributorName = answers.contributorName || '';
+    var contributorEmail = answers.contributorEmail || '';
+    var gitUrl = answers.repo || gitInfo[0] || '';
 
     packageJson.name = slugify(answers.name);
     packageJson.description = answers.description;
     packageJson.contributors.push({
-      name: gitInfo[0],
-      email: gitInfo[1]
+      name: contributorName,
+      email: contributorEmail
     });
-    packageJson.repository.url = gitInfo[2];
+    packageJson.repository.url = gitUrl;
     if (answers.port) {
       packageJson.config.port = answers.port;
     }
 
     bowerJson.name = slugify(answers.name);
     bowerJson.description = answers.description;
-    bowerJson.authors.push(gitInfo[0] + ' <' + gitInfo[1] + '>');
+    bowerJson.authors.push(contributorName + ' <' + contributorEmail + '>');
 
     readmeMd = readmeMd.replace('{{projectName}}', answers.name)
       .replace('{{projectDescription}}', answers.description)
       .replace('{{appPort}}', answers.port);
 
+    if(answers.tests) {
+      packageJson.scripts.test = 'karma start ./test/karma.conf.js --single-run';
+      packageJson.scripts['test:watch'] = 'karma start ./test/karma.conf.js';
+      bowerJson.dependencies['angular-mocks'] = '1.5.0';
+      readmeMd += '\n' +
+        '## Testing\n' +
+        '\n' +
+        'Testing-related stuff is located in the `./test` folder:\n' +
+        '\n' +
+        '* `./test/fixtures` - json fixtures, handled by [https://www.npmjs.com/package/karma-json-fixtures-preprocessor](karma-json-fixtures-preprocessor module)\n' +
+        '* `./test/mocks` - put your mocked components here\n' +
+        '* `./test/specs` - put your actual unit tests here\n' +
+        '\n' +
+        '```bash\n' +
+        '# Run unit tests once\n' +
+        'npm test\n' +
+        '\n' +
+        '# Or make it automatically run on every code change\n' +
+        'npm run test:watch\n' +
+        '```\n' +
+        '\n' +
+        'Read about unit testing of Angular apps in (https://docs.angularjs.org/guide/unit-testing)[the official guide].\n' +
+        '\n';
+    }
+
     if (!~process.argv.indexOf('--dry-run')) {
       if(answers.tests) {
         fs.mkdirSync('./test');
-        packageJson.scripts.test = 'karma start ./test/karma.conf.js --single-run';
-        packageJson.scripts['test:watch'] = 'karma start ./test/karma.conf.js';
-        bowerJson.dependencies['angular-mocks'] = '1.5.0';
-        readmeMd += '\n' +
-          '## Testing\n' +
-          '\n' +
-          'Testing-related stuff is located in the `./test` folder:\n' +
-          '\n' +
-          '* `./test/fixtures` - json fixtures, handled by [https://www.npmjs.com/package/karma-json-fixtures-preprocessor](karma-json-fixtures-preprocessor module)\n' +
-          '* `./test/mocks` - put your mocked components here\n' +
-          '* `./test/specs` - put your actual unit tests here\n' +
-          '\n' +
-          '```bash\n' +
-          '# Run unit tests once\n' +
-          'npm test\n' +
-          '\n' +
-          '# Or make it automatically run on every code change\n' +
-          'npm run test:watch\n' +
-          '```\n' +
-          '\n' +
-          'Read about unit testing of Angular apps in (https://docs.angularjs.org/guide/unit-testing)[the official guide].\n' +
-          '\n';
       }
       fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2), 'utf8');
       fs.writeFileSync('./bower.json', JSON.stringify(bowerJson, null, 2), 'utf8');
       fs.writeFileSync('./README.md', readmeMd, 'utf8');
     } else {
-      console.log(answers, gitInfo);
+      console.log(gitInfo, gitUrl);
+      console.log(answers);
+      console.log(packageJson);
+      console.log(bowerJson);
+      console.log('\n\nREADME.md\n', readmeMd);
     }
 
     console.log('\n\x1b[32mProject setup is done. Installing project dependencies...\x1b[0m\n');
